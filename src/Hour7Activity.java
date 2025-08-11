73563d3ce0912e0a2d610cdba0bc0d32a22b903f
Alisha Carrington <alishasc2001@icloud.com>
2025-08-11 15:38:50 +0100

    Hour 7 - Using Conditional Tests to Make Decisions

---
+++ src/Hour7Activity.java
@@ -0,0 +1,49 @@
+public class Hour7Activity {
+    public static void main(String[] args) {
+        int grade = 63;
+
+        // if statements
+//        if (grade >= 70) {
+//            System.out.println("A - Amazing!");
+//        } else if (grade >= 60) {
+//            System.out.println("B - Beautiful");
+//        } else if (grade >= 50) {
+//            System.out.println("C - Concerning");
+//        } else if (grade >= 40) {
+//            System.out.println("D - Dreadful");
+//        } else {
+//            System.out.println("F - Failed :(");
+//        }
+
+        // switch statement
+        char letterGrade;
+        if (grade >= 70) {
+            letterGrade = 'A';
+        } else if (grade >= 60) {
+            letterGrade = 'B';
+        } else if (grade >= 50) {
+            letterGrade = 'C';
+        } else if (grade >= 40) {
+            letterGrade = 'D';
+        } else {
+            letterGrade = 'F';
+        }
+
+        switch (letterGrade) {
+            case 'A':
+                System.out.println("A - Amazing!");
+                break;
+            case 'B':
+                System.out.println("B - Beautiful");
+                break;
+            case 'C':
+                System.out.println("C - Concerning");
+                break;
+            case 'D':
+                System.out.println("D - Dreadful");
+                break;
+            case 'F':
+                System.out.println("F - Failed :(");
+        }
+    }
+}
