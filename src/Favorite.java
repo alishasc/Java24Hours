f5577ee8626fd9253de5da341d8d8f683e9c4c04
Alisha Carrington <alishasc2001@icloud.com>
2025-08-11 14:04:51 +0100

    Hour 6 - Using Strings to Communicate

---
+++ src/Favorite.java
@@ -0,0 +1,8 @@
+public class Favorite {
+    public static void main(String[] args) {
+        String favorite = "chainsaw";
+        String guess = "pool cue";
+        System.out.println("Is Fin's favorite weapon a " + guess + "?");
+        System.out.println("Answer: " + favorite.equals(guess));
+    }
+}
